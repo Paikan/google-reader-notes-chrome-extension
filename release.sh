7dbfd14fda98a80abdf586329e94fadea4f88fa7
Benjamin Devèze <benjamin.deveze@gmail.com>
2011-06-14 00:18:55 +0200

    Improve release script

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@
 BUILDNAME=GoogleReaderNotes.zip
 BASEDIR=$(dirname $0)
 cd $BASEDIR
-zip -r $BUILDNAME . -x \*.git\* .DS_Store release.sh \*.zip \*~
+zip -r $BUILDNAME . -x \*.git\* \*.DS_Store release.sh \*.zip \*~
